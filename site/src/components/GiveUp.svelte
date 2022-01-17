91670cfbb7ed773819737fcc1ddb73245cda2508
hsull <>
2022-01-17 21:08:07 +1100

    Clean GiveUp.svelte

--- site/src/components/GiveUp.svelte
+++ site/src/components/GiveUp.svelte
@@ -1,16 +1,12 @@
 <script lang="ts">
-    import type { User } from "firebase/auth";
     import { fade } from 'svelte/transition';
 
-    export let doLogin: () => void;
-    export let doSignOut: () => void;
-    export let user: User; 
     export let word: string | undefined;
 </script>
 
 <div id="answer-modal">
 <h1 > The Answer Is:</h1>
-<div class="guess-wrapper">{word}</div>
+<div class="answer-wrapper">{word}</div>
 </div>
 
 <style>
@@ -19,13 +15,7 @@
         text-align: center;
     }
 
-    p {
-        color:  var(--color-primary);
-        margin: 1em;
-    }
-
-
-    .guess-wrapper {
+    .answer-wrapper {
         color: var(--color-accent-light);
         text-align: center;
         text-transform: uppercase;
@@ -34,7 +24,7 @@
     }
 
     @media (max-width: 350px) {
-        .guess-wrapper {
+        .answer-wrapper {
             font-size: 2em;
         }
     }
